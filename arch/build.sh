7666a98d09c98eb033b19930021a31492b4117dc
Mateusz Juda <mateusz.juda@{ii.uj.edu.pl,gmail.com}>
2020-07-24 12:25:42 +0200

    .

--- arch/build.sh
+++ arch/build.sh
@@ -2,10 +2,24 @@
 
 set -e
 
-ver="1.9.3"
+ver="1.9.5"
 registry="registry.capdnet.ii.uj.edu.pl:5000/capd/dev:${ver}"
 docker build . -t "${registry}"
+
+path="/mnt/remote/capdnet_shared/cloud/singularity/"
+
+if [ ! -e "$path" ]; then
+   path="$PWD"
+fi
+
+sing_img="${path}/capd_dev-${ver}.sif"
+echo "Creating ${sing_img}"
+
+#export SINGULARITY_LOCALCACHEDIR=$HOME/tmp 
+#export SINGULARITY_TMPDIR=$HOME/tmp
+#export DOCKER_TMPDIR=$HOME/tmp
+
+singularity build --sandbox ${sing_img} "docker-daemon://${registry}"
+
 docker push  "${registry}"
 
-sing_img="/mnt/remote/capdnet_shared/cloud/singularity/capd_dev-${ver}.img"
-SINGULARITY_TMPDIR=$HOME/tmp singularity build ${sing_img} "docker://${registry}"
